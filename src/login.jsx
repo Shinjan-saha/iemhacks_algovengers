89073a1ab09e9338b75a5c59d079e8b245109b2c
Shinjan saha <116014977+Shinjan-saha@users.noreply.github.com>
2023-08-27 00:31:17 +0530

    Update login.jsx

--- src/login.jsx
+++ src/login.jsx
@@ -1,12 +1,24 @@
 import React, { useContext, useEffect, useState } from "react";
 import "./login.css";
-import { useNavigate } from "react-router-dom";
+import logo from "./images/logo.png";
+import { useFetcher, useNavigate } from "react-router-dom";
 
 import { Link } from "react-router-dom";
 
 import { getUserdata, logIn } from "./firebase";
 import NoteContext from "./context/NoteContext";
 const LoginForm = () => {
+  useEffect(() => {
+    console.log("hello");
+    console.log(a.user);
+    if (a.user) {
+      if (a.user.Category === "Teachers") {
+        navigate("/TeacherDashboard/TeacherDashboard");
+      } else if (a.user.Category === "Students") {
+        navigate("/StudentDashboard/StudentDashboard");
+      }
+    }
+  });
   const navigate = useNavigate();
   // Storing the data of the user which has logged in
   const a = useContext(NoteContext);
@@ -23,24 +35,35 @@ const LoginForm = () => {
     } catch (error) {
       console.log("Error not a reg user");
     }
-    let userinfo = await getUserdata(uid);
+    getUserdata(uid).then((userinfo) => {
+      console.log(userinfo);
+      console.log("before set user");
+      //  a.updateUser(userinfo[0], userinfo[1], userinfo[2], userinfo[3])
+      a.setUser({
+        Category: userinfo[0],
+        CollegeName: userinfo[2],
+        CollegeId: userinfo[1],
+        email: userinfo[3],
+      });
+      sessionStorage.setItem("Category", userinfo[0]);
+      sessionStorage.setItem("CollegeName", userinfo[2]);
+      sessionStorage.setItem("CollegeId", userinfo[1]);
+      sessionStorage.setItem("email", userinfo[3]);
 
-    await a.updateUser(userinfo[0], userinfo[1], userinfo[2], userinfo[3]);
-
-    let category = userinfo[0];
-    if (category === "Teachers") {
-      navigate("/TeacherDashboard/TeacherDashboard");
-    } else if (category === "Students") {
-      navigate("/StudentDashboard/StudentDashboard");
-    }
+      console.log("after setuser");
+      console.log(a.user);
+    });
   };
 
   return (
     <div className="loggin-container">
       <div className="box-login">
         <div className="form">
-          <h2>LoginForm</h2>
           <form onSubmit={handleFormSubmit}>
+            <div className="login-logo-container">
+              <img src={logo} alt="Logo" className="login-logo" />
+            </div>
+            <h2 className="login-form-heading">LoginForm</h2>
             <div className="inputBox-login">
               <input type="text" name="email" required="required" />
               <span>Email</span>
@@ -62,5 +85,4 @@ const LoginForm = () => {
     </div>
   );
 };
-
 export default LoginForm;
