ed4013d02933a4f4758d3973977ae1646bd5ac6f
Aftab <aftabalamdlm@gamil.com>
2023-08-26 17:10:38 +0530

    Added home page and required images

---
+++ src/pages/Home.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "./home.css";
+import logo from "../images/logo.png";
+import homeImg from "../images/raggin-img-1.jpg";
+import signInIcon from '../images/signIn-icon.svg'
+
+export default function Home() {
+
+  return (
+    <div className="container">
+      <div className="navbar">
+        <div className="logo-container">
+          <img src={logo} alt="logo" className="logo" />
+        </div>
+		<div className="nav-btn-container">
+			<button className="signIn-btn">
+			<img src={signInIcon} alt="" className="signin-icon" />
+			<span className="signInSpan">SignUp</span>
+			</button>
+		</div>
+      </div>
+      <main className="main">
+        <div className="text-container">
+          <div className="slogans-container">
+            <h1>Educate, Elevate, <span>Eradicate Ragging.</span></h1>
+            <h1>Say no to Ragging.</h1>
+			<p  className="para">Ragging incidents are often underreported due to fear of retaliation, social stigma, and a lack of awareness about reporting mechanisms.</p>
+			<p className="para">Lets stand together against <span className="highlight">Ragging!</span></p>
+			<h3>Join us in this journey solving this problem!!</h3>
+          </div>
+
+          <div className="btn-container">
+            <button className="btn login-btn">Login</button>
+			<button className="btn aboutUs-btn">About Us</button>
+          </div>
+        </div>
+        <div className="img-container">
+          <img src={homeImg} alt="" className="home-image" />
+        </div>
+      </main>
+    </div>
+  );
+}
